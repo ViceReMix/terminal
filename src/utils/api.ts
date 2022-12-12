4fbbe81b53c2ed74e856f2cb5b2c2291449a1b80
ViceRemix <104978817+ViceReMix@users.noreply.github.com>
2022-12-12 16:50:10 +0200

    btc api V2

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -31,5 +31,7 @@ export const getQuote = async () => {
 
 const getBtcprice = async() => {
   const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
-  return data;
+  return {
+    btcprice: `“${data.usd}”`,
+  };
 }
